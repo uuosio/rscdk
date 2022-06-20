04c0ede59f90f82b217f6ef06f386d3bbbcfc838
learnforpractice <learnforpractice@gmail.com>
2022-06-20 14:35:29 +0800

    Use `cargo test` to run code generator tests

--- crates/codegen/src/contract.rs
+++ crates/codegen/src/contract.rs
@@ -1513,21 +1513,131 @@ impl Contract {
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn test_contract() {
+    fn test_bad_struct() {
         let contract = Contract::new(
-            syn::parse_quote! { #[ink(invalid)] },
+            syn::parse_quote! {},
             syn::parse_quote! {
                 mod hello {
+                    struct AAA_ {
+                        value: u64,
+                    }
+                }        
+            }
+        );
+        assert!(contract.is_err(), "bad return");
+        assert!(contract.err().unwrap().to_compile_error().to_string().contains("struct name with `_` does not supported by contract"));
+    }
 
+    #[test]
+    fn test_dumplciated_action_name() {
+        let contract = Contract::new(
+            syn::parse_quote! {},
+            syn::parse_quote! {
+                mod hello {
+                    #[chain(main)]
+                    pub struct Hello {
+                        receiver: Name,
+                        first_receiver: Name,
+                        action: Name,
+                    }
+                
+                    impl Hello {
+                        pub fn new(receiver: Name, first_receiver: Name, action: Name) -> Self {
+                            Self {
+                                receiver: receiver,
+                                first_receiver: first_receiver,
+                                action: action,
+                            }
+                        }
+                
+                        #[chain(action="test")]
+                        pub fn test(&self) {
+                        }
+                
+                        #[chain(action="test")]
+                        pub fn test2(&self) {
+                        }
+                    }
                 }
             }
         );
+        assert!(contract.is_err(), "bad return");
+        assert!(contract.err().unwrap().to_compile_error().to_string().contains("dumplicated action name: test"));
+    }
 
-        // if let Err(err) = &contract {
-        //     println!("{}", err.to_compile_error().to_string());
-        // }
-        assert_eq!(contract.is_ok(), true);
+    #[test]
+    fn test_invalid_action_name() {
+        let contract = Contract::new(
+            syn::parse_quote! {},
+            syn::parse_quote! {
+                mod hello {
+                    #[chain(main)]
+                    pub struct Hello {
+                        receiver: Name,
+                        first_receiver: Name,
+                        action: Name,
+                    }
+                
+                    impl Hello {
+                        pub fn new(receiver: Name, first_receiver: Name, action: Name) -> Self {
+                            Self {
+                                receiver: receiver,
+                                first_receiver: first_receiver,
+                                action: action,
+                            }
+                        }
+                
+                        #[chain(action="test9")]
+                        pub fn test(&self) {
+                        }
+                    }
+                }
+            }
+        );
+        assert!(contract.is_err(), "bad return");
+        assert!(contract.err().unwrap().to_compile_error().to_string().contains("action name contain invalid character(s), valid charaters are a-z & 1-5: test9"));
+    }
+
+    #[test]
+    fn test_bad_table_name() {
+        let contract = Contract::new(
+            syn::parse_quote! {},
+            syn::parse_quote! {
+                mod hello {
+                    #[chain(table="mydata9")]
+                    pub struct MyData {
+                        #[chain(primary)]
+                        a1: u64,
+                    }
+                }
+            }
+        );
+        assert!(contract.is_err(), "bad return");
+        assert!(contract.err().unwrap().to_compile_error().to_string().contains("table name contain invalid character(s), valid charaters are a-z & 1-5: mydata9"));
+    }
+
+    #[test]
+    fn test_dumplicated_table_name() {
+        let contract = Contract::new(
+            syn::parse_quote! {},
+            syn::parse_quote! {
+                mod hello {
+                    #[chain(table="mydata")]
+                    pub struct MyData {
+                        #[chain(primary)]
+                        a1: u64,
+                    }
+                
+                    #[chain(table="mydata")]
+                    pub struct MyData2 {
+                        #[chain(primary)]
+                        a1: u64,
+                    }
+                }
+            }
+        );
+        assert!(contract.is_err(), "bad return");
+        assert!(contract.err().unwrap().to_compile_error().to_string().contains("dumplicated table name: mydata"));
     }
 }
