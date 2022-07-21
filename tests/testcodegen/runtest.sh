e730966dc8835ee58511fc54d5bfefe9dffd4940
learnforpractice <learnforpractice@gmail.com>
2022-07-21 20:03:17 +0800

    Fix test command

--- tests/testcodegen/runtest.sh
+++ tests/testcodegen/runtest.sh
@@ -1,4 +1,4 @@
 #python3 -m pip install pytest-xdist
-run-ipyeos -m pytest -n 2 -s -x test.py
-#run-ipyeos -m pytest -n 2 -s -x test.py -k test_bad_struct_name
+ipyeos -m pytest -n 2 -s -x test.py
+#ipyeos -m pytest -n 2 -s -x test.py -k test_bad_struct_name
 
